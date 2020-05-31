88621cfb52c7485316b774ad59fb323d026228da
Yusuf Saifee <zyusufsaifee@gmail.com>
2020-05-31 18:07:21 +0530

    File to initialize database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO CATEGORY (category_name)VALUES('American');
+INSERT INTO CATEGORY (category_name)VALUES('Italian');
+INSERT INTO CATEGORY (category_name)VALUES('Maxicon');
+INSERT INTO CATEGORY (category_name)VALUES('Fast Food');
+INSERT INTO UNIT_OF_MEASURE (description)VALUES('Teaspoon');
+INSERT INTO UNIT_OF_MEASURE (description)VALUES('Tablespoon');
+INSERT INTO UNIT_OF_MEASURE (description)VALUES('Cup');
+INSERT INTO UNIT_OF_MEASURE (description)VALUES('Pinch');
+INSERT INTO UNIT_OF_MEASURE (description)VALUES('Ounce');
